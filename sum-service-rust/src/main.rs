10332c380bfc9f8c91f9ed6d44760e5448d76f8f
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-01 20:48:32 -0300

    improve docker-compose

--- sum-service-rust/src/main.rs
+++ sum-service-rust/src/main.rs
@@ -35,7 +35,7 @@ async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr = ([0, 0, 0, 0], 3000).into();
     let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
     let server = Server::bind(&addr).serve(service);
     println!("Listening on http://{}", addr);
