a2f8828464cda2b2d4a3a1b7b398dd32b82fc6a5
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-01 19:52:13 -0300

    progress with scala app

--- aggregator-service-scala/run-all-docker-services.sh
+++ aggregator-service-scala/run-all-docker-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --name=div-go   --rm -p 8083:6000 diegopacheco/div-go:v1 & 
-docker run --name=sub-python --rm -p 8081:5000 diegopacheco/sub-python:v1 &
-docker run --name=sum-rust --rm -p 8084:3000 diegopacheco/sum-rust:v1 & 
-docker run --name=mul-java --rm -p 8082:8080 diegopacheco/mul-java:v1 & 
+docker run -d --name=sub-python --rm -p 8081:5000 diegopacheco/sub-python:v1
+docker run -d --name=mul-java   --rm -p 8082:8080 diegopacheco/mul-java:v1 
+docker run -d --name=div-go     --rm -p 8083:6000 diegopacheco/div-go:v1 
+docker run -d --name=sum-rust   --rm -p 8084:3000 diegopacheco/sum-rust:v1 
