22588bf04d94e5031ae0dda361e5f5e6e7416270
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-01 19:46:21 -0300

    working

--- aggregator-service-scala/run-all-docker-services.sh
+++ aggregator-service-scala/run-all-docker-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --name=div-go   --rm -p 6000:8083 diegopacheco/div-go:v1 & 
-docker run --name=sub-python --rm -p 5000:8081 diegopacheco/sub-python:v1 &
-docker run --name=sum-rust --rm -p 3000:8084 diegopacheco/sum-rust:v1 & 
-docker run --name=mul-java --rm -p 8080:8082 diegopacheco/mul-java:v1 & 
+docker run --name=div-go   --rm -p 8083:6000 diegopacheco/div-go:v1 & 
+docker run --name=sub-python --rm -p 8081:5000 diegopacheco/sub-python:v1 &
+docker run --name=sum-rust --rm -p 8084:3000 diegopacheco/sum-rust:v1 & 
+docker run --name=mul-java --rm -p 8082:8080 diegopacheco/mul-java:v1 & 
--- aggregator-service-scala/run.sh
+++ aggregator-service-scala/run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-SUB_PYTHON_IP=$(docker inspect sub-python  | jq -r '.[0].NetworkSettings.IPAddress')
-MUL_JAVA_IP=$(docker inspect mul-java  | jq -r '.[0].NetworkSettings.IPAddress')
-DIV_GO_IP=$(docker inspect div-go  | jq -r '.[0].NetworkSettings.IPAddress')
-SUM_RUST_IP=$(docker inspect sum-rust  | jq -r '.[0].NetworkSettings.IPAddress')
-
-export SUB_SERVICE_URL=http://$SUB_PYTHON_IP:5000/service/sub
-export MUL_SERVICE_URL=http://$MUL_JAVA_IP:8080/service/mul
-export DIV_SERVICE_URL=http://$DIV_GO_IP:6000/service/div
-export SUM_SERVICE_URL=http://$SUM_RUST_IP:3000/service/sum
+export SUB_SERVICE_URL=http://127.0.0.1:8081/service/sub
+export MUL_SERVICE_URL=http://127.0.0.1:8082/service/mul
+export DIV_SERVICE_URL=http://127.0.0.1:8083/service/div
+export SUM_SERVICE_URL=http://127.0.0.1:8084/service/sum
 sbt run
