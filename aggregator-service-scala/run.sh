1eedf8c1993613e0a9341f36f9621c0ca08533c2
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-01 19:31:22 -0300

    working

--- aggregator-service-scala/run.sh
+++ aggregator-service-scala/run.sh
@@ -5,8 +5,8 @@ MUL_JAVA_IP=$(docker inspect mul-java  | jq -r '.[0].NetworkSettings.IPAddress')
 DIV_GO_IP=$(docker inspect div-go  | jq -r '.[0].NetworkSettings.IPAddress')
 SUM_RUST_IP=$(docker inspect sum-rust  | jq -r '.[0].NetworkSettings.IPAddress')
 
-export SUB_SERVICE_URL=http://$SUB_PYTHON_IP:8081/service/sub
-export MUL_SERVICE_URL=http://$MUL_JAVA_IP:8082/service/mul
-export DIV_SERVICE_URL=http://$DIV_GO_IP:8083/service/div
-export SUM_SERVICE_URL=http://$SUM_RUST_IP:8084/service/sum
+export SUB_SERVICE_URL=http://$SUB_PYTHON_IP:5000/service/sub
+export MUL_SERVICE_URL=http://$MUL_JAVA_IP:8080/service/mul
+export DIV_SERVICE_URL=http://$DIV_GO_IP:6000/service/div
+export SUM_SERVICE_URL=http://$SUM_RUST_IP:3000/service/sum
 sbt run
