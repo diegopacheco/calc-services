04b655305ee4e6b5c29ab5566f5187ad58d8ed3b
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-01 17:59:41 -0300

    progress scala

--- aggregator-service-scala/build.sh
+++ aggregator-service-scala/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbt assembly
+sbt clean compile package assembly
--- aggregator-service-scala/src/main/scala/com/App.scala
+++ aggregator-service-scala/src/main/scala/com/App.scala
@@ -14,7 +14,13 @@ object App {
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
 
-     val route =
+    val DIV_SERVICE_URL = System.getenv("DIV_SERVICE_URL")
+    val SUB_SERVICE_URL = System.getenv("SUB_SERVICE_URL")
+    val MUL_SERVICE_URL = System.getenv("MUL_SERVICE_URL")
+    val SUM_SERVICE_URL = System.getenv("SUM_SERVICE_URL")
+    println(s"DIV_SERVICE_URL=${DIV_SERVICE_URL} \n SUB_SERVICE_URL=${SUB_SERVICE_URL} \n MUL_SERVICE_URL=${MUL_SERVICE_URL} \n SUM_SERVICE_URL=${SUM_SERVICE_URL} \n")    
+
+    val route =
       path("service") {
         get {
           parameter("math".as[String]) { (math) =>
