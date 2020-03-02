a82381db5478af69fe9962882d81d73293e3fb38
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-02 12:03:54 -0300

    Added scripts for minikube and kubectl

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-kubectl apply -f specs/
+kubectl apply -f specs/agg-scala/
+kubectl apply -f specs/div-go/
+kubectl apply -f specs/mul-java/
+kubectl apply -f specs/sub-python/
+kubectl apply -f specs/sum-rust/
